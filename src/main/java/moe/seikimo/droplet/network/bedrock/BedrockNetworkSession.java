916b712a996bff96114bdfde49ff5d13000ad9c0
KingRainbow44 <kobedo11@gmail.com>
2023-11-18 01:46:33 -0500

    (feat) Implement sessions for Bedrock network clients

--- src/main/java/moe/seikimo/droplet/network/bedrock/BedrockNetworkSession.java
+++ src/main/java/moe/seikimo/droplet/network/bedrock/BedrockNetworkSession.java
@@ -1,6 +1,34 @@
 package moe.seikimo.droplet.network.bedrock;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import moe.seikimo.droplet.network.NetworkSession;
+import moe.seikimo.droplet.network.shared.BasePacket;
+import org.cloudburstmc.protocol.bedrock.BedrockServerSession;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Getter
+@RequiredArgsConstructor
 public final class BedrockNetworkSession extends NetworkSession {
+    private static final Map<BedrockServerSession, BedrockNetworkSession> sessions
+            = new ConcurrentHashMap<>();
+
+    /**
+     * Get the {@link BedrockNetworkSession} for the given {@link BedrockServerSession}.
+     *
+     * @param session The session.
+     * @return The network session.
+     */
+    public static BedrockNetworkSession from(BedrockServerSession session) {
+        return sessions.computeIfAbsent(session, BedrockNetworkSession::new);
+    }
+
+    private final BedrockServerSession handle;
+
+    @Override
+    public void sendPacket(BasePacket packet) {
+        this.getHandle().sendPacket(packet.toBedrock());
+    }
 }
