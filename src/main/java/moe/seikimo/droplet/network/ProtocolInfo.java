d8f76686eee123d73b06c5ed2239f1e2c6dc1849
KingRainbow44 <kobedo11@gmail.com>
2023-12-21 23:55:19 -0500

    (protocol) Update Bedrock version

--- src/main/java/moe/seikimo/droplet/network/ProtocolInfo.java
+++ src/main/java/moe/seikimo/droplet/network/ProtocolInfo.java
@@ -3,10 +3,10 @@ package moe.seikimo.droplet.network;
 import com.github.steveice10.mc.protocol.codec.MinecraftCodec;
 import com.github.steveice10.mc.protocol.codec.PacketCodec;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
-import org.cloudburstmc.protocol.bedrock.codec.v622.Bedrock_v622;
+import org.cloudburstmc.protocol.bedrock.codec.v630.Bedrock_v630;
 
 public interface ProtocolInfo {
-    BedrockCodec BEDROCK_CODEC = Bedrock_v622.CODEC;
+    BedrockCodec BEDROCK_CODEC = Bedrock_v630.CODEC;
     PacketCodec JAVA_CODEC = MinecraftCodec.CODEC;
 
     int BEDROCK_PROTOCOL = ProtocolInfo.BEDROCK_CODEC.getProtocolVersion();
