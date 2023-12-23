2c871b1714bed75825f420988aaa1295c74e9fbb
KingRainbow44 <kobedo11@gmail.com>
2023-12-23 17:17:01 -0500

    (feat:login) Load a world at start up

--- src/main/java/moe/seikimo/droplet/Server.java
+++ src/main/java/moe/seikimo/droplet/Server.java
@@ -2,14 +2,20 @@ package moe.seikimo.droplet;
 
 import lombok.Getter;
 import lombok.Setter;
+import moe.seikimo.droplet.block.BlockPalette;
+import moe.seikimo.droplet.item.ItemManager;
 import moe.seikimo.droplet.network.NetworkManager;
 import moe.seikimo.droplet.network.bedrock.BedrockInterface;
 import moe.seikimo.droplet.network.java.JavaInterface;
 import moe.seikimo.droplet.utils.objects.Config;
+import moe.seikimo.droplet.world.World;
+import moe.seikimo.droplet.world.biome.Biome;
+import moe.seikimo.droplet.world.io.DropletFormatReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +25,11 @@ public final class Server {
      */
     public static void initialize() {
         Server.instance = new Server();
+
+        // Load server data.
+        BlockPalette.load();
+        ItemManager.load();
+        Biome.load();
     }
 
     @Getter private static Server instance;
@@ -31,6 +42,9 @@ public final class Server {
             = new ArrayList<>();
     @Getter private final Config config
             = Config.read(new File("server.properties"));
+
+    @Getter @Setter private World defaultWorld;
+    @Getter @Setter private ItemManager itemManager;
 
     @Getter @Setter private String ip = "0.0.0.0";
     @Getter @Setter private short bedrockPort = 19132;
@@ -48,6 +62,28 @@ public final class Server {
         // Register network interfaces.
         this.networkManager.registerInterface(new BedrockInterface(this));
         this.networkManager.registerInterface(new JavaInterface(this));
+
+        // Load the default world.
+        this.loadDefaultWorld();
+    }
+
+    /**
+     * Loads the default world from disk.
+     */
+    public void loadDefaultWorld() {
+        try {
+            var worldReader = new DropletFormatReader();
+            this.setDefaultWorld(worldReader.read(new File("world.droplet")));
+
+            this.getLogger().info("Preparing world {}...",
+                    this.getDefaultWorld().getName());
+        } catch (IOException exception) {
+            this.getLogger().error("Failed to read default world file.", exception);
+            System.exit(1);
+        } catch (Exception exception) {
+            this.getLogger().error("Failed to load default world.", exception);
+            System.exit(1);
+        }
     }
 
     /**
