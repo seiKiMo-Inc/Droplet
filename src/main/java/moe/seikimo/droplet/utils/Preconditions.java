bd51ce3a1cf2ec3b0c624af5e65b1003eb3d5b48
KingRainbow44 <kobedo11@gmail.com>
2023-12-22 19:57:11 -0500

    (feat) Add conditional functions

---
+++ src/main/java/moe/seikimo/droplet/utils/Preconditions.java
@@ -0,0 +1,38 @@
+package moe.seikimo.droplet.utils;
+
+import java.io.File;
+
+public interface Preconditions {
+    /**
+     * Checks if the given file exists.
+     *
+     * @param file The file to check.
+     */
+    static void fileExists(File file) {
+        if (!file.exists()) {
+            throw new IllegalArgumentException("File " + file.getAbsolutePath() + " does not exist.");
+        }
+    }
+
+    /**
+     * Checks if the given file is a directory.
+     *
+     * @param file The file to check.
+     */
+    static void canReadFile(File file) {
+        if (!file.canRead()) {
+            throw new IllegalArgumentException("File " + file.getAbsolutePath() + " cannot be read.");
+        }
+    }
+
+    /**
+     * Checks if the given file is a directory.
+     *
+     * @param file The file to check.
+     */
+    static void notDirectory(File file) {
+        if (file.isDirectory()) {
+            throw new IllegalArgumentException("File " + file.getAbsolutePath() + " is a directory.");
+        }
+    }
+}
