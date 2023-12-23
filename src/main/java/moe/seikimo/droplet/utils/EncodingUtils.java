9b96a057ac910e07863ecf23d0b46664899b2216
KingRainbow44 <kobedo11@gmail.com>
2023-12-22 19:56:00 -0500

    (feat:world) Add utilities for decoding positions from numbers

--- src/main/java/moe/seikimo/droplet/utils/EncodingUtils.java
+++ src/main/java/moe/seikimo/droplet/utils/EncodingUtils.java
@@ -1,6 +1,9 @@
 package moe.seikimo.droplet.utils;
 
 import com.google.gson.Gson;
+import moe.seikimo.droplet.world.WorldFormat;
+import moe.seikimo.droplet.world.WorldFormat.ChunkPos;
+import org.cloudburstmc.math.vector.Vector3i;
 
 import java.util.Base64;
 
@@ -27,5 +30,56 @@ public interface EncodingUtils {
     static <T> T base64Decode(String base64, Class<T> type) {
         return EncodingUtils.GSON.fromJson(new String(
                 EncodingUtils.base64Decode(base64)), type);
+    }
+
+    /**
+     * Converts a Java ChunkPos to a Bedrock ChunkPos.
+     *
+     * @param x The x coordinate of the chunk.
+     * @param z The z coordinate of the chunk.
+     * @return A chunk position object.
+     */
+    static ChunkPos convert(int x, int z) {
+        return ChunkPos.newBuilder()
+                .setX(x).setZ(z)
+                .build();
+    }
+
+    /**
+     * Encodes a location to the Droplet format.
+     *
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     * @param z The z coordinate.
+     * @return The encoded location.
+     */
+    static int encodePosition(int x, int y, int z) {
+        return ((x & 0x3FFFFFF) << 6) | ((z & 0x3FFFFFF) << 12) | (y & 0xFFF);
+    }
+
+    /**
+     * Decodes a location from the Droplet format.
+     *
+     * @param encoded The encoded location.
+     * @return The decoded location.
+     */
+    static Vector3i decodePosition(int encoded) {
+        var x = (encoded >> 6) & 0x3FFFFFF;
+        var z = (encoded >> 12) & 0x3FFFFFF;
+        var y = encoded & 0xFFF;
+
+        return Vector3i.from(x, y, z);
+    }
+
+    /**
+     * Compresses an X, Y, Z coordinate into a single index.
+     *
+     * @param x The X coordinate.
+     * @param y The Y coordinate.
+     * @param z The Z coordinate.
+     * @return The index.
+     */
+    static int getIndex(int x, int y, int z) {
+        return ((x << 8) + (z << 4)) | y;
     }
 }
