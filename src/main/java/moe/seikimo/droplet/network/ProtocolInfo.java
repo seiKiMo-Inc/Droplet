3545654be890e068e3d152f363dcf79514500c06
KingRainbow44 <kobedo11@gmail.com>
2024-08-25 16:28:43 -0400

    feat(protocol): Update to Bedrock 1.21.20

--- src/main/java/moe/seikimo/droplet/network/ProtocolInfo.java
+++ src/main/java/moe/seikimo/droplet/network/ProtocolInfo.java
@@ -1,12 +1,12 @@
 package moe.seikimo.droplet.network;
 
+import org.cloudburstmc.protocol.bedrock.codec.v712.Bedrock_v712;
 import org.geysermc.mcprotocollib.protocol.codec.PacketCodec;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
-import org.cloudburstmc.protocol.bedrock.codec.v686.Bedrock_v686;
 import org.geysermc.mcprotocollib.protocol.codec.MinecraftCodec;
 
 public interface ProtocolInfo {
-    BedrockCodec BEDROCK_CODEC = Bedrock_v686.CODEC;
+    BedrockCodec BEDROCK_CODEC = Bedrock_v712.CODEC;
     PacketCodec JAVA_CODEC = MinecraftCodec.CODEC;
 
     int BEDROCK_PROTOCOL = ProtocolInfo.BEDROCK_CODEC.getProtocolVersion();
